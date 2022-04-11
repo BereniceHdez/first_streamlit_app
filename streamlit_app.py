3198bec33c6c5a9d4db6d7b826541537b130550b
Berenice Hernandez <behdezlo@gmail.com>
2022-04-11 20:29:07 +0200

    Update streamlit_app.py
    Favorites word

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 
 streamlit.title('My Mom\'s New Healthy Diner')
 
-streamlit.header('Breakfast Favrites')
+streamlit.header('Breakfast Favorites')
 
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
