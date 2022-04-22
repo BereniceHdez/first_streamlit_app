9145db079ca14dde9d2e81e64b6d6fcb0685640b
Berenice Hernandez <behdezlo@gmail.com>
2022-04-22 15:20:25 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,9 +43,11 @@ my_data_row = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_row)
 
-#display the table on the page
-streamlit.dataframe(fruits_to_show)
 #New Section to display fruityvice api response 
 streamlit.header('Fruityvice Fruit Advice!')
 fruit_choice = streamlit.text_input('What fruit would you like information about?' , 'Kiwi')
 streamlit.write('The user entered', fruit_choice)
+
+
+import requests 
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
