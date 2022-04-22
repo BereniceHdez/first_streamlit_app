582ed36b52c768330b8679ba093019c1e06f2a53
Berenice Hernandez <behdezlo@gmail.com>
2022-04-22 20:45:04 +0200

    Update streamlit_app.py
    Added header

--- streamlit_app.py
+++ streamlit_app.py
@@ -52,6 +52,7 @@ def get_fruit_load_list():
           return my_cur.fetchall()
      
 # Add a button to load the fruit
+streamlit.header('View Our Fruit List - Add your favories!')
 if streamlit.button('Get Fruit Load List'):
      my_data_rows = get_fruit_load_list()
      streamlit.dataframe(my_data_rows)
